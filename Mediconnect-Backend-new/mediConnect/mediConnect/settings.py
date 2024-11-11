0becb00a3e92c23475a52e3617a750ae1a7c4fb0
Chamath Goonetilleke <31180821+Chamath-Goonetilleke@users.noreply.github.com>
2024-11-11 23:35:13 +0530

    pub

--- Mediconnect-Backend-new/mediConnect/mediConnect/settings.py
+++ Mediconnect-Backend-new/mediConnect/mediConnect/settings.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'mediConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'mediconnect.DB017',
-#         'HOST': 'mediconnect.c9o2aky687e4.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mediconnect.DB017',
+        'HOST': 'mediconnect.c9o2aky687e4.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
